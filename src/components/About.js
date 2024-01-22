aa985effbc5f011a6d0092281667434f2a765a8b
Tai Nguyen-Trung (Teth Adam) <tai.nguyentrung1996@gmail.com>
2024-01-22 19:17:06 +0700

    Add About/Contact/Services/Work

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,89 @@
 import React from "react";
+// CountUp
+import CountUp from "react-countup";
+// intersection observer hook
+import { useInView } from "react-intersection-observer";
+// motion
+import { motion } from "framer-motion";
+// varian
+import { fadeIn } from "../variants";
 
 const About = () => {
+	const [ref, inView] = useInView({
+		threshold: 0.5,
+	});
 	return (
-		<div className="section" id="about">
-			About
-		</div>
+		<seciton className="section" id="about" ref={ref}>
+			<div className="container mx-auto">
+				<div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+					{/* img */}
+					<motion.div
+						variants={fadeIn("right", 0.3)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+					></motion.div>
+					{/* text */}
+					<motion.div
+						variants={fadeIn("left", 0.5)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1"
+					>
+						<h2 className="h2 text-accent">About me</h2>
+						<h3 className="h3 mb-4">
+							I'm a Freelance Front-end Developer with over 5 years of
+							experience.
+						</h3>
+						<p className="mb-6">
+							Irure labore tempor ex aliqua et in mollit est. Tempor id Lorem
+							nulla nisi ipsum duis irure. Elit quis do non nostrud culpa do
+							commodo reprehenderit enim excepteur tempor incididunt labore. Et
+							sit proident ad veniam laborum sit in ea laborum laboris ipsum
+							fugiat. Est voluptate dolore duis occaecat. Eu est sit do tempor.
+							Duis commodo officia anim incididunt sint quis.
+						</p>
+						{/* stats */}
+						<div className="flex gap-x-6 lg:gap-x-10 mb-12">
+							<div>
+								<div className="text-[40px] font-tertiary text-gradient mb-2">
+									{inView ? <CountUp start={0} end={13} duration={3} /> : null}
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Years of <br /> Experience
+								</div>
+							</div>
+							<div>
+								<div className="text-[40px] font-tertiary text-gradient mb-2">
+									{inView ? <CountUp start={0} end={5} duration={3} /> : null}{" "}
+									k+
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Projects <br /> Completed
+								</div>
+							</div>
+							<div>
+								<div className="text-[40px] font-tertiary text-gradient mb-2">
+									{inView ? <CountUp start={0} end={7} duration={3} /> : null}{" "}
+									k+
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Satisfied <br /> Clients
+								</div>
+							</div>
+						</div>
+						<div className="flex gap-x-8 items-center">
+							<button className="btn btn-lg">Contact me</button>
+							<a href="#" className="text-gradient btn-link">
+								My portfolio
+							</a>
+						</div>
+					</motion.div>
+				</div>
+			</div>
+		</seciton>
 	);
 };
 
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,104 @@
-import React from 'react';
+import React from "react";
+
+// motion
+import { motion } from "framer-motion";
+// varians
+import { fadeIn } from "../variants";
+// image
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+	return (
+		<section className="section" id="work">
+			<div className="container mx-auto">
+				<div className="flex flex-col lg:flex-row gap-x-10">
+					<motion.div
+						variants={fadeIn("right", 0.3)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+					>
+						{/* text */}
+						<div>
+							<h2 className="h2 leading-tight text-accent">
+								My Latest <br /> Work.
+							</h2>
+							<p className="max-w-sm mb-16">
+								Voluptate Lorem minim sit consectetur occaecat ea incididunt.
+								Laboris quis enim deserunt ipsum adipisicing duis pariatur
+								eiusmod nostrud voluptate laboris occaecat laborum.
+							</p>
+							<button className="btn btn-sm">View all projects</button>
+						</div>
+						{/* image */}
+						<div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+							{/* overlay */}
+							<div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+							<img
+								className="group-hover:scale-125 transition-all duration-500"
+								src={Img1}
+								alt=""
+							/>
+							{/* pretitle */}
+							<di className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+								<span className="text-gradient">UI/Ux Design</span>
+							</di>
+							{/* title */}
+							<div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50">
+								<span className="text-3xl text-white">Project Title</span>
+							</div>
+						</div>
+					</motion.div>
+					<motion.div
+						variants={fadeIn("left", 0.2)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1 flex flex-col gap-y-10"
+					>
+						{/* image */}
+						<div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+							{/* overlay */}
+							<div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+							<img
+								className="group-hover:scale-125 transition-all duration-500"
+								src={Img2}
+								alt=""
+							/>
+							{/* pretitle */}
+							<di className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+								<span className="text-gradient">UI/Ux Design</span>
+							</di>
+							{/* title */}
+							<div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50">
+								<span className="text-3xl text-white">Project Title</span>
+							</div>
+						</div>
+						<div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+							{/* overlay */}
+							<div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+							<img
+								className="group-hover:scale-125 transition-all duration-500"
+								src={Img3}
+								alt=""
+							/>
+							{/* pretitle */}
+							<di className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+								<span className="text-gradient">UI/Ux Design</span>
+							</di>
+							{/* title */}
+							<div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50">
+								<span className="text-3xl text-white">Project Title</span>
+							</div>
+						</div>
+					</motion.div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Work;
